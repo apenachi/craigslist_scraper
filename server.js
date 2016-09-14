1b3e792892691c25337f0e1a331f6e3244811310
apenachi <apenachi@yahoo.com>
2016-09-13 23:05:33 -0400

    craigslist scraper

--- server.js
+++ server.js
@@ -12,8 +12,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // mongoose.connect('mongodb://localhost/craigslist-scraper');
+var dbUrl = process.env.MONGODB_URI || 'mongodb://heroku_b8mzml2k:7evkgr8g85kjrh28nefeetm8g4@ds029426.mlab.com:29426/heroku_b8mzml2k'
+mongoose.connect(dbUrl, function() {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + dbUrl + ' . ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + dbUrl);
+  }
+});
 
-mongoose.connect(process.env.MONGODB_URI);
 var db = mongoose.connection;
 
 // db.on('error', function(err) {
